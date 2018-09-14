07308c08415def00f85759695e90095eaff45b40
Shymaxer <yushkevuch1337@gmail.com>
2018-09-14 18:14:07 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -4,4 +4,20 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+  var array = [0];
+  preferences = array.concat(preferences);
+  var count = 0;
+
+  for(var i =1 ; i<= preferences.length; i++)
+  {
+    var a = preferences[i];
+    var b = preferences[a];
+    var c = preferences[b];
+
+    if((c == i) && (preferences[a]!= a) && (preferences[b] != b)  && (preferences[c] != c)){
+      count ++
+    }
+  }
+    return count/3
 };
